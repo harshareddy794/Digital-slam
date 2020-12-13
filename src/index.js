e776ff42c7741f55f86cc8d2a4a32a1f38a1af8a
harshareddy794 <harshareddy794@gmail.com>
2020-12-13 22:23:52 +0530

    Added landing page

--- src/index.js
+++ src/index.js
@@ -3,15 +3,22 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
-// 
-
-import ownerRoutes from "./routes/owner.js"
+// Imporing routes
+import ownerRoutes from "./routes/owner.js";
 
 // Middleware use
+app.use(express.static("public"))
+app.set("view engine","ejs")
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/owner",ownerRoutes.router)
+// Home route
+app.get("/",async(req,res)=>{
+    res.render()
+});
+
+// Routes configuration
+app.use("/owner",ownerRoutes.router);
 
 
 // Server listiningt
