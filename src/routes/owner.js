c5a1efd0ed9193fca5e99d1412865c27e5c91970
harshareddy794 <harshareddy794@gmail.com>
2020-12-17 20:39:20 +0530

    Added signup page

--- src/routes/owner.js
+++ src/routes/owner.js
@@ -4,10 +4,9 @@ const router = express.Router();
 import controller from "../controller/owner.js"
 
 
-router.get("/signup",
-    
-    controller.createOwner,
-);
+router.get("/signup", (req,res) =>{
+    res.render("signup");
+});
 
 
 export default{
