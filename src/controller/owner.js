152945178604bde427a3a27bdbef6675744c30f3
harshareddy794 <harshareddy794@gmail.com>
2020-12-28 21:54:26 +0530

    Added user sigtnup pag

--- src/controller/owner.js
+++ src/controller/owner.js
@@ -1,11 +1,22 @@
 import service from "../service/owner.js";
 
-const createOwner = async(req,res) => {
-    service.createOwner(req);
-    res.send("hello")
+const createOwnerGET = async(req,res) => {
+    res.render("signup");
 };
 
 
+const createOwnerPOST = async(req,res) => {
+    const result = await service.createOwner(req);
+    res.send(result);
+};
+
+const loginOwner = () => {
+
+}
+
+
 export default{
-    createOwner,
+    createOwnerGET,
+    createOwnerPOST,
+    loginOwner,
 }
--- src/service/owner.js
+++ src/service/owner.js
@@ -1,5 +1,17 @@
-const createOwner = (req) => {
-    console.log(req)
+import ownerModel from "../models/owner.js"
+import bcrypt from "bcryptjs";
+
+
+const createOwner = async({body}) => {
+    try {
+        body.password = await bcrypt.hash(body.password,10);
+        const owner = new ownerModel.ownerModel({...body})
+        const result = await owner.save();
+        console.log("Result ",result) 
+        return result;
+    } catch (error) {
+        throw new Error(error);
+    }
 }
 
 export default{
