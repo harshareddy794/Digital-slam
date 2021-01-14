86f9548437c6e34b72a50863de4ed90c3c34a7c0
harshareddy794 <harshareddy794@gmail.com>
2021-01-14 20:13:03 +0530

    Added joi schema validation

--- src/middleware/joiSchemaValidation.js
+++ src/middleware/joiSchemaValidation.js
@@ -14,6 +14,7 @@ const validateObjectSchema = (data, schema) => {
   return null;
 };
 
+
 const validateBody = (schema) => {
   return (req, res, next) => {
     let response = { ...constant.defaultServerResponse };
