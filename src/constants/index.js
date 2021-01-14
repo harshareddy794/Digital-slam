9ab4410cb2a981e55cc74a4bff65046e3803f7d1
harshareddy794 <harshareddy794@gmail.com>
2021-01-14 20:11:04 +0530

    Added constants

---
+++ src/constants/index.js
@@ -0,0 +1,35 @@
+export default {
+    userMessage: {
+      SUCCESS_MESSAGE: "User created successfully",
+      USER_NOT_FOUND: "User not found",
+      USER_FETCHED: "User Fetched",
+      USER_DELETED: "User Deleted",
+      USER_UPDATED: "User Updated",
+      DUPLICATE_EMAIL: "User exist with given email",
+      LOGIN_SUCCESS: "Login success",
+      LOGIN_FAILED: "Login Failed",
+    },
+    clientMessage: {
+      SUCCESS_MESSAGE: "Client created successfully",
+      USER_NOT_FOUND: "Client not found",
+      USER_FETCHED: "Client Fetched",
+      USER_DELETED: "Client Deleted",
+      USER_UPDATED: "Client Updated",
+      DUPLICATE_EMAIL: "Client exist with given email",
+      LOGIN_SUCCESS: "Login success",
+      LOGIN_FAILED: "Login Failed",
+    },
+    passwordMessage: {
+      PASSWORD_SUCCESS: "Password set success",
+      PASSWORD_FAILED: "Password set failed",
+    },
+  
+    requestValidationMessage: {
+      BAD_REQUEST: "Invalid fields",
+      TOKEN_MISSING: "Token missing from header",
+    },
+    databaseMessage: {
+      INVALID_ID: "Invalid Id",
+    },
+  };
+  
---
+++ src/middleware/joiSchemaValidation.js
@@ -0,0 +1,47 @@
+import constant from "../constants/index";
+
+const validateObjectSchema = (data, schema) => {
+  const validation = schema.validate(data, { convert: false });
+  if (validation.error) {
+    const errorDetails = validation.error.details.map((value) => {
+      return {
+        error: value.message,
+        path: value.path,
+      };
+    });
+    return errorDetails;
+  }
+  return null;
+};
+
+const validateBody = (schema) => {
+  return (req, res, next) => {
+    let response = { ...constant.defaultServerResponse };
+    const error = validateObjectSchema(req.body, schema);
+    if (error) {
+      response.body = error;
+      response.message = constant.requestValidationMessage.BAD_REQUEST;
+      return res.status(response.status).send(response);
+    }
+    return next();
+  };
+};
+
+const validateQueryParams = (schema) => {
+  return (req, res, next) => {
+    let response = { ...constant.defaultServerResponse };
+    const error = validateObjectSchema(req.query, schema);
+    if (error) {
+      response.body = error;
+      response.message = constant.requestValidationMessage.BAD_REQUEST;
+      return res.status(response.status).send(response);
+    }
+    return next();
+  };
+};
+
+export default {
+  validateQueryParams,
+  validateBody,
+  validateObjectSchema,
+};
